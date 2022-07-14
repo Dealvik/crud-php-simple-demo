1c9e993885d51631c2a99b7dea9ea5f256686d95
David Kofman <dealvik@gmail.com>
2022-07-14 12:47:05 -0400

    change test

--- index.php
+++ index.php
@@ -8,31 +8,37 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // usin
 ?>
 
 <html>
-<head>	
+
+<head>
 	<title>Homepage</title>
 </head>
 
 <body>
-<a href="add.html">Add New Data</a><br/><br/>
+
+	<h2>Homepage</h2>
+
+
+	<a href="add.html">Add New Data</a><br /><br />
 
 	<table width='80%' border=0>
 
-	<tr bgcolor='#CCCCCC'>
-		<td>Name</td>
-		<td>Age</td>
-		<td>Email</td>
-		<td>Update</td>
-	</tr>
-	<?php 
-	//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
-	while($res = mysqli_fetch_array($result)) { 		
-		echo "<tr>";
-		echo "<td>".$res['name']."</td>";
-		echo "<td>".$res['age']."</td>";
-		echo "<td>".$res['email']."</td>";	
-		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
-	}
-	?>
+		<tr bgcolor='#CCCCCC'>
+			<td>Name</td>
+			<td>Age</td>
+			<td>Email</td>
+			<td>Update</td>
+		</tr>
+		<?php
+		//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
+		while ($res = mysqli_fetch_array($result)) {
+			echo "<tr>";
+			echo "<td>" . $res['name'] . "</td>";
+			echo "<td>" . $res['age'] . "</td>";
+			echo "<td>" . $res['email'] . "</td>";
+			echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
+		}
+		?>
 	</table>
 </body>
+
 </html>
